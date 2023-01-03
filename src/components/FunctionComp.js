8f0a41484381679491230ee273470eb4433e5482
biswasarprajapati <121846569+biswasarprajapati@users.noreply.github.com>
2023-01-03 11:53:25 +0530

    Text manipulation website

---
+++ src/components/FunctionComp.js
@@ -0,0 +1,4 @@
+import { React } from "react";
+function FunctionalComp(){
+    return <p>This is a Functional componect</p>
+}
---
+++ src/components/TextForm.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+    const handleUpClick = () => {
+        // console.log("Uppercase was clicked" + text);
+        let newText = text.toUpperCase()
+        setText(newText)
+        props.showAlert("converted to uppercase","success");
+    }
+    const handleLoClick = () => {
+        // console.log("Uppercase was clicked" + text);
+        let newText = text.toLowerCase()
+        setText(newText)
+        props.showAlert("converted to lowercase","success");
+    }
+    const handleClearClick = () => {
+        let newText = ""
+        setText(newText)
+        props.showAlert("text cleard","success");
+    }
+    const handleOnChange = (event) => {
+        // console.log("On change");
+        setText(event.target.value)
+    }
+    const handleCopy = () => {
+        var text = document.getElementById("myBox")
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        props.showAlert("Copied to Clipboard","success");
+    }
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+        props.showAlert("Remove extra spaces","success");
+    }
+
+    const [text, setText] = useState('');
+    //text = "new text"; //Wrong way to change the state
+    //settext = "new text"; //Correct way to change the state
+    return (
+        <>
+            <div className='container' style={{color:props.mode === 'dark'? 'white':'#042743'}}>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode === 'dark'? 'grey':'white', color:props.mode === 'dark'? 'white':'#042743'}} id="myBox" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-1" onClick={handleUpClick} >Conver to Uppercase</button>
+                <button className="btn btn-primary mx-1" onClick={handleLoClick} >Conver to Lowercase</button>
+                <button className="btn btn-primary mx-1" onClick={handleClearClick} >Clear Text</button>
+                <button className="btn btn-primary mx-1" onClick={handleCopy} >Copy Text</button>
+                <button className="btn btn-primary mx-1" onClick={handleExtraSpaces} >Remove Extra Spaces</button>
+            </div>
+            <div className="container my-3" style={{color:props.mode === 'dark'? 'white':'#042743'}}>
+                <h2>Your text summery</h2>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").length} Minutes read</p>
+                <h2>Preview</h2>
+                <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
+            </div>
+        </>
+    )
+}
